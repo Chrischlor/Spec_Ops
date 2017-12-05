8791a81e52cafb0349b8b790caf8db266bf9ab23
Rune Brøgger Jepsen <rune4977@edu.eal.dk>
2017-12-05 14:13:32 +0100

    test2

--- Brugergrænseflade/Brugergrænseflade/Conection.cs
+++ Brugergrænseflade/Brugergrænseflade/Conection.cs
@@ -11,7 +11,7 @@ namespace Brugergrænseflade
     {
         public void SqlAdgang (string strconn,SqlConnection conn)
         {
-            
+            //test
              strconn = @"hej med dig ;)";
         }
             
